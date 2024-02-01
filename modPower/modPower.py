6de93115c17d16a44c7b4602d6f2f99e5a5ee429
Asher <136246043+Ashermuzic@users.noreply.github.com>
2024-02-01 11:09:05 -0900

    Create modPower.py

---
+++ modPower/modPower.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+
+power = a ** b
+mod = power % c
+
+print(power)
+print(mod)
