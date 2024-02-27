6e4775d6f70ba10f241d5706be01a76c8d496cf2
Asher <136246043+Ashermuzic@users.noreply.github.com>
2024-02-27 11:55:52 -0900

    Time: 0 ms (100%), Space: 2 MB (50.43%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.go
+++ 2469-convert-the-temperature/2469-convert-the-temperature.go
@@ -1,8 +1,3 @@
 func convertTemperature(celsius float64) []float64 {
-    var result []float64
-    
-    result = append(result, celsius + 273.15)
-    result = append(result, celsius * 1.80 + 32.00)
-
-    return result
+    return []float64{celsius + 273.15, celsius * 1.80 + 32.00}
 }
