db75992eb29035f9c3a5428911545a1755e493f1
Asher <136246043+devasherr@users.noreply.github.com>
2024-06-06 09:27:46 -0900

    Time: 39 ms (43.73%), Space: 16.8 MB (29.27%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,21 +2,17 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        # 1 2 3
         curSet = []
         def dfs(i):
             if i >= len(nums):
                 res.append(curSet.copy())
-                return 
+                return
 
-            # decision to include
             curSet.append(nums[i])
             dfs(i + 1)
 
-            # decision not to include
             curSet.pop()
             dfs(i + 1)
 
         dfs(0)
-
         return res
