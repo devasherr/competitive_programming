4d43fc20d3b89342af0ef69c3c65d60497a3eec6
Asher <136246043+devasherr@users.noreply.github.com>
2024-07-02 09:08:07 -0900

    Time: 35 ms (69.04%), Space: 16.5 MB (25.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,5 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = int("".join(map(str, digits)))
-        s += 1
-        return [int(c) for c in str(s)]
+        res = []
+        carry = 1
+        i = len(digits) - 1
+
+        while i >= 0 or carry:
+            num = digits[i] if i >= 0 else 0
+            num += carry
+
+            res.append(num % 10)
+            carry = num // 10
+            i -= 1
+
+        return reversed(res)
