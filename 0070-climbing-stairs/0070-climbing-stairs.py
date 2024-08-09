c601a83f5d3c7ff6c84b385fc0e688a3d67cb3f9
Asher <136246043+devasherr@users.noreply.github.com>
2024-08-09 01:41:12 -0900

    Time: 35 ms (52.05%), Space: 16.5 MB (14.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,10 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        def backtrack(n, memo):
-            if n == 0:
-                return 1
-            if n < 0:
-                return 0
+    def climbStairs(self, n: int, memo = {}) -> int:
+            if n == 0: return 1
+            if n < 0: return 0
             if n in memo:
                 return memo[n]
-            memo[n-1] = backtrack(n-1, memo)
-            memo[n-2] = backtrack(n-2, memo)
+
+            memo[n-1] = self.climbStairs(n-1, memo)
+            memo[n-2] = self.climbStairs(n-2, memo)
             return memo[n-1] + memo[n-2]
-        
-        return backtrack(n, {})
