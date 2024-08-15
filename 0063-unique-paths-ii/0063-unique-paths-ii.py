b18e74ba3e1240382d02b83b2da1ca0739b010ff
Asher <136246043+devasherr@users.noreply.github.com>
2024-08-15 08:53:33 -0900

    Time: 47 ms (36.31%), Space: 16.7 MB (46.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        n = len(obstacleGrid)
-        for i in range(n):
-            for j in range(n):
+        rows, cols = len(obstacleGrid), len(obstacleGrid[0])
+        for i in range(rows):
+            for j in range(cols):
                 if obstacleGrid[i][j] == 1:
                     obstacleGrid[i][j] = -1
         
         if obstacleGrid[0][0] != -1: obstacleGrid[0][0] = 1
 
-        for i in range(n):
-            for j in range(n):
+        for i in range(rows):
+            for j in range(cols):
                 if obstacleGrid[i][j] == -1:
                     continue
                 
-                if j + 1 < n and obstacleGrid[i][j+1] != -1: obstacleGrid[i][j+1] += obstacleGrid[i][j]
-                if i + 1 < n and obstacleGrid[i+1][j] != -1: obstacleGrid[i+1][j] += obstacleGrid[i][j]
+                if j + 1 < cols and obstacleGrid[i][j+1] != -1: obstacleGrid[i][j+1] += obstacleGrid[i][j]
+                if i + 1 < rows and obstacleGrid[i+1][j] != -1: obstacleGrid[i+1][j] += obstacleGrid[i][j]
 
         print(obstacleGrid)
         return obstacleGrid[-1][-1] if  obstacleGrid[-1][-1] != -1 else 0
