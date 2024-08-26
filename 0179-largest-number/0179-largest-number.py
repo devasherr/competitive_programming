b386787667013a4ca3d0d32e8f88618ce10172df
Asher <136246043+devasherr@users.noreply.github.com>
2024-08-25 21:30:54 -0900

    Time: 45 ms (31.22%), Space: 16.6 MB (33.29%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,8 +1,10 @@
-class LargerNumKey(str):
-    def __lt__(x, y):
-        return x + y > y + x
-
 class Solution:
-    def largestNumber(self, nums):
-        largest_num = "".join(sorted(map(str, nums), key=LargerNumKey))
-        return "0" if largest_num[0] == "0" else largest_num
+    def largestNumber(self, nums: List[int]) -> str:
+        def custom_compare(x, y):
+            if x + y > y + x: return -1
+            if x + y < y + x: return 1
+            return 0
+
+        nums = [str(n) for n in nums]
+        nums = sorted(nums, key=functools.cmp_to_key(custom_compare))
+        return "0" if nums[0] == "0" else "".join(nums)
