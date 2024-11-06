6b54a6b1931e5b529f00ed3b2044956c6928e0e8
Asher <136246043+devasherr@users.noreply.github.com>
2024-11-06 10:08:35 -0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,24 +1,22 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # make all postive
         n = len(nums)
-
+        # clean
         for i in range(n):
             if nums[i] <= 0 or nums[i] > n:
                 nums[i] = n + 1
-        
-        # now the question is [1-n]
+
+        # -neg index
         for i in range(n):
             num = abs(nums[i])
-
-            # we need all to less than n and be negative
-            if num > n:
+            if num == n + 1:
                 continue
-            if nums[num - 1] > 0:
-                nums[num - 1] *= -1
-        
+
+            nums[num-1] *= -1
+               
+        # find
         for i in range(n):
             if nums[i] > 0:
-                return i + 1
+                return i+1
 
         return n + 1
