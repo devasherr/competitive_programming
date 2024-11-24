19882d256bbb888847a9e24352fe8dbcdc7672cf
Asher <136246043+devasherr@users.noreply.github.com>
2024-11-24 10:35:24 -0900

    Time: 1998 ms (17.78%), Space: 29.1 MB (56.51%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,38 +1,18 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         rows, cols = len(box), len(box[0])
-        matrix = [[0 for _ in range(cols)] for _ in range(rows)]
-
         for i in range(rows):
-            count = 0
-            for j in range(cols):
-                if box[i][j] == "*":
-                    count = 0
-                    matrix[i][j] = "*"
-                    continue
-                
+            last = cols - 1
+            for j in reversed(range(cols)):
                 if box[i][j] == "#":
-                    count += 1
-                if j - 1 >= 0 and matrix[i][j - 1] != "*":
-                    matrix[i][j - 1] = 0
-                matrix[i][j] = count
+                    box[i][j], box[i][last] = box[i][last], box[i][j]
+                    last -= 1
+                elif box[i][j] == "*":
+                    last = j - 1
 
-        res = [[0 for _ in range(rows)] for _ in range(cols)]
-        output = [["." for _ in range(rows)] for _ in range(cols)]
-
+        res = [["" for _ in range(rows)] for _ in range(cols)]
         for j in range(cols):
             for i in range(rows):
-                res[j][rows - i - 1] = matrix[i][j]
-
-        for i in range(len(res) - 1, -1, -1):
-            for j in range(len(res[0])):
-                if res[i][j] == "*":
-                    output[i][j] = "*"
-                elif res[i][j] > 0:
-                    k = i
-                    for _ in range(res[i][j]):
-                        if k >= 0 and output[k][j] != "*":
-                            output[k][j] = "#"
-                            k -= 1
-
-        return output
+                res[j][rows - i - 1] = box[i][j]
+                
+        return res
