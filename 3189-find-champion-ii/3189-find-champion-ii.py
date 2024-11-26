b59547b34d5f4b4cb90bcd582122a55212846c73
Asher <136246043+devasherr@users.noreply.github.com>
2024-11-26 08:50:10 -0900

    Time: 15 ms (34.44%), Space: 18.4 MB (9.65%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,27 +1,12 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        indegree = [0 for _ in range(n)]
         for u, v in edges:
-            graph[u].append(v)
-
-        champ = [True for _ in range(n)]
-
-        def bfs(node):
-            q = deque([node])
-            while q:
-                curNode = q.popleft()
-                for child in graph[curNode]:
-                    if champ[child]:
-                        q.append(child)
-                        champ[child] = False
-
-        for i in range(n):
-            if champ[i]:
-                bfs(i)
+            indegree[v] += 1
         
         res = -1
         for i in range(n):
-            if champ[i]:
+            if indegree[i] == 0:
                 if res != -1:
                     return -1
                 res = i
