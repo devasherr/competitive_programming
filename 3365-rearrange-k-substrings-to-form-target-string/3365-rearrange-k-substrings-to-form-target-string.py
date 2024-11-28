313c4101ad8240193c96f54872f74d762c66e747
Asher <136246043+devasherr@users.noreply.github.com>
2024-11-27 22:03:58 -0900

    Time: 188 ms (64.09%), Space: 24.8 MB (56.67%) - LeetHub

--- 3365-rearrange-k-substrings-to-form-target-string/3365-rearrange-k-substrings-to-form-target-string.py
+++ 3365-rearrange-k-substrings-to-form-target-string/3365-rearrange-k-substrings-to-form-target-string.py
@@ -1,24 +1,10 @@
 class Solution:
     def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
-        n = len(s)
-        words = defaultdict(int)
+        sMap, tMap = defaultdict(int), defaultdict(int)
+        jump = len(s) // k
+        
+        for i in range(0, len(s) - jump + 1, jump):
+            sMap[s[i:i+jump]] += 1
+            tMap[t[i:i+jump]] += 1
 
-        i = 0
-        while i < n:
-            words[s[i:i+(n // k)]] += 1
-            i += (n // k)
-
-        i = 0
-        while True:
-            start = i
-
-            for word in words:
-                if words[word] < 1:
-                    continue
-                if word == t[i:i+len(word)]:
-                    i += len(word)
-                    words[word] -= 1
-            if i == start:
-                return False
-            if i >= len(t):
-                return True
+        return sMap == tMap
