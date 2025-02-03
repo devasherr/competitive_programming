f640e5a5617436cb2a242c9a1c21b19897ae0dbf
Asher <136246043+devasherr@users.noreply.github.com>
2025-02-03 12:38:45 +0300

    Time: 6 ms (25.39%), Space: 18.4 MB (67.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,19 +1,27 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        rowSet, colSet = set(), set()
-        N, M = len(matrix), len(matrix[0])
+        rows, cols = len(matrix), len(matrix[0])
+        firstRow = any([matrix[0][i] == 0 for i in range(cols)])
+        firstCol = any([matrix[i][0] == 0 for i in range(rows)])
 
-        for i in range(N):
-            for j in range(M):
+        for i in range(1, rows):
+            for j in range(1, cols):
                 if matrix[i][j] == 0:
-                    rowSet.add(i)
-                    colSet.add(j)
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
         
-        for i in rowSet:
-            for j in range(M):
+        for i in range(1, rows):
+            if matrix[i][0] != 0: continue
+            for j in range(1, cols):
+                matrix[i][j] = 0
+        for j in range(1, cols):
+            if matrix[0][j] != 0: continue
+            for i in range(1, rows):
                 matrix[i][j] = 0
         
-        for j in colSet:
-            for i in range(N):
-                matrix[i][j] = 0
-                
+        if firstRow:
+            for j in range(cols):
+                matrix[0][j] = 0
+        if firstCol:
+            for i in range(rows):
+                matrix[i][0] = 0
