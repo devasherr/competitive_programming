69c3eb96ed8f044fa6af84f0a9367aef779a549f
Asher <136246043+devasherr@users.noreply.github.com>
2025-02-07 22:32:12 +0300

    Time: 124 ms (59.86%), Space: 63.6 MB (24.02%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,27 +1,19 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        w = []
-        l = []
-        winnerMap = defaultdict(int)
-        loserMap = defaultdict(int)
+        winnerMap, loserMap = {}, {}
 
-        # winner and loser hashmap
-        for m in matches:
-            winnerMap[m[0]] += 1
-            loserMap[m[1]] += 1
+        for winner, loser in matches:
+            loserMap[loser] = loserMap.get(loser, 0) + 1
+            winnerMap[winner] = winnerMap.get(winner, 0) + 1
 
-        # winner if not in loser
-        for p in winnerMap:
-            if p not in loserMap:
-                w.append(p)
-
-        # loser if count is 1
-        for p in loserMap:
-            if loserMap[p] == 1:
-                l.append(p)
-
-        # they have to be sorted
-        w.sort()
-        l.sort()
+        goats = []
+        for key in winnerMap:
+            if loserMap.get(key, 0) == 0:
+                goats.append(key)
+            
+        averages = []
+        for key in loserMap:
+            if loserMap[key] == 1:
+                averages.append(key)
         
-        return [w, l]
+        return [sorted(goats), sorted(averages)]
