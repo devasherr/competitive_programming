2848b142e85a86cacf8c18623acec218730d9f11
Asher <136246043+devasherr@users.noreply.github.com>
2025-03-08 12:23:36 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,21 +1,37 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        def leftSection(i):
-            if i - 1 >= 0 and arr[i - 1] < arr[i]:
-                return leftSection(i - 1) + 1
+        # 3,s1,i2,p3,2,ej1,1,i2,3,4,5,4,3,2,1
+        # 5 
+        # startMin = 1
+        if len(arr) < 3:
             return 0
             
-        def rightSection(i):
-            if i + 1 < len(arr) and arr[i] > arr[i + 1]:
-                return rightSection(i + 1) + 1
-            return 0
+        j = 0
+        i = 0
+        n = len(arr)
+        startMin = 0
+        peak = 0
+        endMin = 0
+        mountain = 0
+        # get start min
+        while i < n:
+            i = j + 1
+            while i < n:
+                if arr[i] <= arr[i - 1]:
+                    startMin = i
+                    break
+                i += 1
 
-        res = 0
+            # get peak
+            j = startMin
+            while j + 1 < n and arr[j] < arr[j + 1]:
+                j += 1
+            peak = j
 
-        # leave first and second to last of checking purpose
-        for i in range(1, len(arr) - 1):
-            if not (arr[i - 1] < arr[i] > arr[i + 1]):
-                continue
-            
-            res = max(res, leftSection(i) + rightSection(i) + 1)
-        return res
+            # get end min
+            while j + 1 < n and arr[j] > arr[j + 1]:
+                j += 1
+            endMin = j
+            mountain = max(mountain, j - i + 1)
+
+        return endMin - startMin + 1 if endMin != startMin else 0
