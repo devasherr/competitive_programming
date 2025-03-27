2347bfcdfed9720957f2550bb209b207a5e8db09
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-03-27 21:57:30 +0300

    Time: 112 ms (28.22%), Space: 34.7 MB (24.26%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -3,11 +3,7 @@ class Solution:
         rightCount = Counter(nums)
         leftCount = defaultdict(int)
 
-        dom, domCount = 0, 0
-        for key in rightCount:
-            if rightCount[key] > domCount:
-                dom = key
-                domCount = rightCount[key]
+        dom = max(rightCount, key=rightCount.get)
         
         for i in range(len(nums)):
             leftCount[nums[i]] += 1
