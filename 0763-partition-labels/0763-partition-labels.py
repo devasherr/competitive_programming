d89125167e51edaca0780db2194944347d756426
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-03-30 18:49:00 +0300

    Time: 3 ms (91.15%), Space: 17.5 MB (97.48%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,22 +1,16 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastIndexMap = {}
-        for i in range(len(s) -1, -1, -1):
-            if s[i] not in lastIndexMap:
-                lastIndexMap[s[i]] = i
-        
-        res = []
-        count = 0
-        endIndex = 1
-
+        rightIndex = defaultdict(int)
         for i in range(len(s)):
-            count += 1
-            if i == endIndex:
-                res.append(count)
-                count = 0
-                continue
-            
-            if lastIndexMap[s[i]] > endIndex:
-                endIndex = lastIndexMap[s[i]] 
+            rightIndex[s[i]] = i
+        
+        prev, right = 0, 0
+        res = []
 
+        for left in range(len(s)):
+            right = max(right, rightIndex[s[left]])
+            if right == left:
+                res.append(right - prev + 1)
+                prev = right + 1
+            
         return res
