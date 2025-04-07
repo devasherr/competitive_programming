91078dbb21505bb5e67fbf1c954c23e30f967622
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-04-07 22:20:31 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        def backtrack(i, aSum, bSum, memo):
+        def backtrack(i, aSum, bSum):
             if i == len(nums):
                 return aSum == bSum
-
-            if (aSum, bSum) in memo:
-                return memo[(aSum, bSum)]
-
-            memo[(aSum, bSum)] = backtrack(i+1, aSum+nums[i], bSum, memo) or backtrack(i+1, aSum, bSum+nums[i], memo)
-            return memo[(aSum, bSum)]
-            
-        return backtrack(0, 0, 0, {})
+                
+            return backtrack(i+1, aSum+nums[i], bSum) or backtrack(i+1, aSum, bSum+nums[i])
+        return backtrack(0, 0, 0)
