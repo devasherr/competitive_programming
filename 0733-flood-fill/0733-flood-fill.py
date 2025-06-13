d82eb5f592d0ebddf19bb017b9800f9d1feaeedf
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-06-13 23:08:04 +0300

    Time: 0 ms (100%), Space: 18.1 MB (27.46%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        targetColor = image[sr][sc]
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def isInBound(i, j):
-            return 0 <= i < len(image) and 0 <= j < len(image[0]) 
+        inBound = lambda x, y : 0 <= x < len(image) and 0 <= y < len(image[0])
 
-        def dfs(i, j):
-            if image[i][j] != targetColor:
-                return
+        q = deque([(sr, sc)])
+        original = image[sr][sc]
 
+        while q:
+            i, j = q.popleft()
             image[i][j] = color
-            
+
             for dr, dc in directions:
-                if isInBound(i+dr, j+dc) and image[i+dr][j+dc] != color:
-                    dfs(i+dr, j+dc)
-        
-        dfs(sr, sc)
+                if inBound(i+dr, j+dc) and image[i+dr][j+dc] == original and image[i+dr][j+dc] != color:
+                    q.append((i+dr, j+dc))
         return image
