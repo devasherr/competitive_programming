db75648d23f124548426856c7885a12857562ff4
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-07-28 11:29:10 +0300

    Time: 0 ms (100%), Space: 18 MB (12.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,11 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        # if if odd and add 0
-        # two pointers
-        # compare
-        if len(nums) % 2 != 0:
-            nums.append(0)
+    def rob(self, nums) -> int:
+        def dp(i):
+            if i >= len(nums): return 0
 
-        l, r = 0, 1
-        lSum = 0
-        rSum = 0
+            if i not in memo:
+                memo[i] = max(nums[i] + dp(i+2), dp(i+1))
+            return memo[i]
 
-        while r <= len(nums) - 1:
-            lSum += nums[l]
-            rSum += nums[r]
-
-            l += 2
-            r += 2
-
-        return max(lSum, rSum)
+        memo = {}
+        return dp(0)
