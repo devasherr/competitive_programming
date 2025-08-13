df4dcfcc1dbd7edb195ecfac71246d51fe226ee8
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-08-13 10:47:02 +0300

    Time: 16 ms (13.39%), Space: 21.3 MB (90.09%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.heap = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        heapq.heappush(self.heap, val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        self.heap.remove(val)
+        heapq.heapify(self.heap)
+        return val
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.heap[0]
