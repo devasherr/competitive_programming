f20c29c86d0cf30efe993c8970594a99b2c03314
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-08-14 16:36:18 +0300

    Time: 0 ms (100%), Space: 18 MB (69.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,20 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def generate(openCount, closeCount, path):
-            if not openCount and not closeCount:
-                res.append("".join(path))
-                return
-            if openCount > 0: 
-                path.append("(")
-                generate(openCount-1, closeCount, path)
-                path.pop()
-    
-            if openCount < closeCount:
-                path.append(")")
-                generate(openCount, closeCount-1, path)
-                path.pop()
-        generate(n, n, [])
-        return res
-            
+    def generateParenthesis(self, n: int):
+        self.res = []
+        def generate(open_count, close_count, cur):
+            if open_count > n: return []
+            if open_count == n and close_count == n:
+                self.res.append("".join(cur[::]))
+                return 
+
+            cur.append("(")
+            generate(open_count+1, close_count, cur)
+            cur.pop()
+
+            if open_count > close_count:
+                cur.append(")")
+                generate(open_count, close_count+1, cur)
+                cur.pop()
+
+        generate(0, 0, [])
+        return self.res
