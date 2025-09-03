5f67d9524d71191cef30ca79d354b85481417a66
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-09-03 10:07:29 +0300

    Time: 132 ms (46.64%), Space: 16.5 MB (100%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,24 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        def check(count, k):
-            first = 1
-            for key in count:
-                if first:
-                    first = 0
-                    continue
-                if count[key] > k: return False
-                k -= count[key]
-            return True
-
-        count = defaultdict(int)
+        count = [0 for _ in range(26)]
         left, res = 0, 0
-        for right in range(len(s)):
-            count[s[right]] += 1
-            while not check(count, k):
-                count[s[left]] -= 1
-                if count[s[left]] == 0:
-                    del count[s[left]]
-                left += 1
 
+        for right in range(len(s)):
+            count[ord(s[right]) - ord('A')] += 1
+            while (right - left + 1) - max(count) > k:
+                count[ord(s[left])- ord('A')] -= 1
+                left += 1
+            
             res = max(res, right - left + 1)
         return res
