3fc1862a7edd4b2cdecf4b5640e2bab0d1bb6b8c
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-09-05 11:41:20 +0300

    Time: 507 ms (11.58%), Space: 18.4 MB (33.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,22 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        res = s + "a"
-        left = 0
+        def check(sCount, tCount):
+            for key in tCount:
+                if sCount[key] < tCount[key]:
+                    return False
+            return True
 
         sCount, tCount = defaultdict(int), Counter(t)
+        left = 0
+        res, resLength = "", float("inf")
         for right in range(len(s)):
             sCount[s[right]] += 1
-            while self.checkGood(tCount, sCount):
-                if right - left + 1 < len(res):
+            while check(sCount, tCount):
+                if right - left + 1 < resLength:
                     res = s[left:right+1]
+                    resLength = right - left + 1
 
                 sCount[s[left]] -= 1
                 left += 1
-                
-        return res if len(res) < len(s) + 1 else ""
 
-    def checkGood(self, tCount, sCount):
-        for key in tCount:
-            if sCount[key] < tCount[key]:
-                return False
-        return True
+        return res
