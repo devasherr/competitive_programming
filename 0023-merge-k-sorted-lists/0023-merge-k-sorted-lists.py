cfdf6c939bbc08b5d6a65774a696ed17a21d0820
Asher Samuel <136246043+devasherr@users.noreply.github.com>
2025-09-15 10:32:44 +0300

    Time: 39 ms (19.11%), Space: 25.9 MB (5.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,22 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        vals = []
-        for l in lists:
-            cur = l
-            while cur:
-                vals.append(cur.val)
-                cur = cur.next
-
-        vals.sort(reverse=True)
-
-        dummy = ListNode(0)
+    def mergeTwoLists(self, l1, l2):
+        if not l2: return l1
+        dummy = ListNode()
         tail = dummy
-
-        while vals:
-            node = ListNode(vals.pop())
-            tail.next = node
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                tail.next = ListNode(l2.val)
+                l2 = l2.next
             tail = tail.next
+        if l1: tail.next = l1
+        if l2: tail.next = l2
 
         return dummy.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+        n = len(lists)
+        interval = 1
+
+        while interval < n:
+            for i in range(0, n, interval*2):
+                firstHalf = lists[i]
+                secondHalf = lists[i+interval] if i + interval < n else []
+                lists[i] = self.mergeTwoLists(firstHalf, secondHalf)
+            interval *= 2
+
+        return lists[0]
